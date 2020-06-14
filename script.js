d388a1a7332fdae3e201ced2d4e4c1a9832ec0bb
pedrokorwa <korwapedro@gmail.com>
2020-06-14 16:24:18 +0700

    tambahan dom

---
+++ script.js
@@ -0,0 +1,23 @@
+const ganti = document.getElementById("tombol");
+ganti.addEventListener("click", function () {
+  document.body.style.backgroundColor = ["black"];
+});
+
+const gantiSatu = document.getElementById("tombol1");
+gantiSatu.addEventListener("click", function () {
+  document.body.style.backgroundColor = "blue";
+});
+const gantiDua = document.getElementById("tombol2");
+gantiDua.addEventListener("click", function () {
+  document.body.style.backgroundColor = "green";
+});
+
+const gantiTiga = document.getElementById("tombol3");
+gantiTiga.addEventListener("click", function () {
+  document.body.style.backgroundColor = "red";
+});
+
+const gantiEmpat = document.getElementById("tombol4");
+gantiEmpat.addEventListener("click", function () {
+  document.body.style.backgroundColor = "white";
+});
