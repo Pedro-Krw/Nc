067206774149db9e835fbfd2beb4fe743f5b813c
Pedro'Korwa <59316805+Pedro-korwa@users.noreply.github.com>
2020-06-19 21:21:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+
+alert("Hai Ada Fitur Baru Kami Yaitu Dark Mode");
+// fitur baru Dark Mode
+const satu = document.getElementById("satu");
 const ganti = document.getElementById("tombol");
 ganti.addEventListener("click", function () {
-  document.body.style.backgroundColor = "darkcyan";
+  document.body.style.backgroundColor = "black";
+  satu.style.color = "white";
 });
 
 const gantiSatu = document.getElementById("tombol1");
@@ -20,13 +25,15 @@ gantiTiga.addEventListener("click", function () {
 const gantiEmpat = document.getElementById("tombol4");
 gantiEmpat.addEventListener("click", function () {
   document.body.style.backgroundColor = "white";
+  satu.style.color = "black";
 });
-let body = document.querySelector("body");
-let eneble = document.getElementById("eneble");
-var darkMode = false;
 
-eneble.addEventListener("click", function () {
-  darkMode = !darkMode;
-  body.classList.add("dark-mode");
-  body.classList.toggle("dark-mode");
-});
+// let body = document.querySelector("body");
+// let eneble = document.getElementById("eneble");
+// var darkMode = false;
+
+// eneble.addEventListener("click", function () {
+//   darkMode = !darkMode;
+//   body.classList.add("dark-mode");
+//   body.classList.toggle("dark-mode");
+// });
