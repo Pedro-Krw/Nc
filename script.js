1eb5f8e0fb54cc3791d4f4b77eed1c298286af3a
pedrokorwa <korwapedro@gmail.com>
2020-06-24 21:04:00 +0700

    Update Timing Event

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 const ganti = document.getElementById("tombol");
 ganti.addEventListener("click", function () {
-  document.body.style.backgroundColor = "darkcyan";
-  satu.style.color = "white";
+  document.body.classList.add("darkcyan");
+  setTimeout(function () {
+    document.body.classList.remove("darkcyan");
+  }, 500);
+  // document.body.style.backgroundColor = "darkcyan";
+  // satu.style.color = "white";
 });
 
 const gantiSatu = document.getElementById("tombol1");
 gantiSatu.addEventListener("click", function () {
-  document.body.style.backgroundColor = "blue";
+  document.body.classList.add("blue");
+  setTimeout(function () {
+    document.body.classList.remove("blue");
+  }, 500);
 });
 const gantiDua = document.getElementById("tombol2");
 gantiDua.addEventListener("click", function () {
-  document.body.style.backgroundColor = "green";
+  document.body.classList.add("green");
+  setTimeout(function () {
+    document.body.classList.remove("green");
+  }, 500);
 });
 
 const gantiTiga = document.getElementById("tombol3");
 gantiTiga.addEventListener("click", function () {
-  document.body.style.backgroundColor = "red";
+  document.body.classList.add("red");
+  setTimeout(function () {
+    document.body.classList.remove("red");
+  }, 500);
 });
 
 const gantiEmpat = document.getElementById("tombol4");
 gantiEmpat.addEventListener("click", function () {
-  document.body.style.backgroundColor = "white";
-  satu.style.color = "black";
+  document.body.classList.add("none");
+  setTimeout(function () {
+    document.body.classList.remove("none");
+  }, 500);
 });
 
 // ?ini bagian tombol dark mode dan mengunakan perulangan for dan forEach
